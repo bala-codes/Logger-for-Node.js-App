e9dd3839b2d23b4d830de881f86c204577889064
Balakrishna Kumar V <balakrishnakumar.v@gmail.com>
2021-08-04 20:17:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const { Logger } = require("./logger");
-let clogger = new Logger(1,"general_logs.txt",max_length=3);
+let clogger = new Logger(save_length = 1,file_name = "general_logs.txt", max_length = 3);
 
 clogger.log('Log Level - 1');
 clogger.log('Log Level - 2');
 clogger.log('Log Level - 3');
 clogger.log('Log Level - 4');
 clogger.log('Log Level - 5');
-clogger.log('Log Level - 6');
-clogger.log('Log Level - 7');
-clogger.log('Log Level - 8');
-clogger.log('Log Level - 9');
-clogger.log('Log Level - 10');
